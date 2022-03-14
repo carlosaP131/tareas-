b0ffd86286d52fa36115b736a95464b5c08cf04e
carlos <carlos.cantrax303@gmail.com>
2022-03-14 12:58:54 -0600

    tarea hacer una tabla con print donde se muestre la longitud de los datos

---
+++ src/principal/Principal.java
@@ -0,0 +1,32 @@
+package principal;
+
+public class Principal {
+	public static void main(String args[]) {
+		// primitivos
+		int numero;
+		float numero2;
+		byte numero3;
+		short numero4;
+		long numero5;
+		double numero6;
+		boolean numero7;
+		char letra;
+		numero = 234;
+		numero2 = (float)34.5;
+		numero3 = -34;
+		numero4 = (short)-32.5;
+		numero5 = -2;
+		numero6 = 10.0;
+		numero7 = false;
+		letra = 65;
+		System.out.println("el número es :" + numero);
+		System.out.println("el número es :" + numero2);
+		System.out.println("el número es :" + numero3);
+		System.out.println("el número es :" + numero4);
+		System.out.println("el número es :" + numero5);
+		System.out.println("el número es :" + numero6);
+		System.out.println("el número es :" + numero7);
+		System.out.println("el número es :" + letra);
+	}
+
+}
